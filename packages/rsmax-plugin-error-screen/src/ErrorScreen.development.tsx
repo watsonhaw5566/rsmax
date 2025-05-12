b71fa3750b496baac493544414542fcd89f68c9b
wangjue <348748267@qq.com>
2025-05-12 22:07:47 +0800

    fix(rsmax/plugin-error-screen): 修复 plugin-error-screen 依赖关系

--- packages/rsmax-plugin-error-screen/src/ErrorScreen.development.tsx
+++ packages/rsmax-plugin-error-screen/src/ErrorScreen.development.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import RedBox from '@rsmax/redbox-react';
 
 interface Props {
--- packages/rsmax-plugin-error-screen/src/ErrorScreen.tsx
+++ packages/rsmax-plugin-error-screen/src/ErrorScreen.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View } from '@rsmax/one';
+import * as React from 'react';
+import { View } from 'rsmax/one';
 
 const styles = {
   screen: {
