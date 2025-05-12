823437c5dbf111c846172668664fbcea21dc3e21
wangjue <348748267@qq.com>
2025-05-12 22:40:42 +0800

    test(runtime): 移除 todo

--- packages/rsmax-runtime/vitest.config.js
+++ packages/rsmax-runtime/vitest.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    // todo 这里的 cjs 包存在问题
     exclude: ['cjs/**/*', 'esm/**/*', 'node_modules/**/*'],
   },
 });
