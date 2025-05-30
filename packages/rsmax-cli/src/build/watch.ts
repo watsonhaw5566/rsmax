9baaf3e496eda8bb0895c2e93daf57b517fbbeba
watsonhaw5566 <348748267@qq.com>
2025-05-30 15:20:21 +0800

    fix(cli): 修热监听失效问题

--- packages/rsmax-cli/src/build/watch.ts
+++ packages/rsmax-cli/src/build/watch.ts
@@ -3,14 +3,27 @@ import Builder from './Builder';
 
 let isRunning = true;
 
-export default function watch(builder: Builder, watcher: any) {
+export default function watch(builder: Builder, watcher: any, addEntry = false) {
   // 监听额外的文件
+  const { entries } = builder.entryCollection;
   chokidar
     .watch([`${builder.options.rootDir}/app.config.{js,ts}`, `${builder.options.rootDir}/theme.config.{js,ts}`], {
       cwd: builder.options.cwd,
     })
     .on('change', () => {
       if (isRunning) return;
+      if (addEntry) {
+        builder.fetchProjectConfig();
+        builder.fetchProjectThemeConfig();
+        builder.entryCollection.init();
+        const nextEntries = builder.entryCollection.entries;
+        nextEntries.forEach(entry => {
+          if (!entries.get(entry.filename)) {
+            entry.virtualModule.apply(builder.webpackCompiler);
+            entry.updateSource(true);
+          }
+        });
+      }
       watcher.invalidate();
     });
 
