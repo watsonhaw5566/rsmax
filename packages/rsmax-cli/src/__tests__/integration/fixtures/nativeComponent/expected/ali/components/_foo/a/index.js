6bd098c7678b76e561f0ef73919b6bdce83defdc
watsonhaw5566 <348748267@qq.com>
2025-06-11 23:50:44 +0800

    test(cli): 修正 component-scripts 的引用

--- packages/rsmax-cli/src/__tests__/integration/fixtures/nativeComponent/expected/ali/components/_foo/a/index.js
+++ packages/rsmax-cli/src/__tests__/integration/fixtures/nativeComponent/expected/ali/components/_foo/a/index.js
@@ -1,6 +1,6 @@
 require('./../../../runtime.js');
 (my["webpackChunk"] = my["webpackChunk"] || []).push([["1"], {
-82: (function () {
+85: (function () {
 Component({});
 
 
@@ -8,7 +8,7 @@ Component({});
 
 },function(__webpack_require__) {
 var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId) }
-var __webpack_exports__ = (__webpack_exec__(82));
+var __webpack_exports__ = (__webpack_exec__(85));
 
 }
 ]);
