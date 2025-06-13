a8db20e4d5698a047675c132e4def46224ba8e25
watsonhaw5566 <348748267@qq.com>
2025-06-13 16:30:47 +0800

    test(cli): 更新测试文件

---
+++ packages/rsmax-cli/src/__tests__/integration/fixtures/web/expected/pages/index/index2.js
@@ -0,0 +1,51 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["2"], {
+210: (function (__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+__webpack_require__.r(__webpack_exports__);
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* export default binding */ __WEBPACK_DEFAULT_EXPORT__)
+});
+/* ESM import */var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(11);
+/* ESM import */var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* ESM import */var rsmax__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(207);
+/* ESM import */var rsmax__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(rsmax__WEBPACK_IMPORTED_MODULE_1__);
+/* ESM import */var _index_css_modules__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(208);
+/* ESM import */var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(209);
+/* ESM import */var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__);
+
+
+
+
+/* ESM default export */ function __WEBPACK_DEFAULT_EXPORT__(props) {
+    console.log(props);
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(rsmax__WEBPACK_IMPORTED_MODULE_1__.View, {
+        className: _index_css_modules__WEBPACK_IMPORTED_MODULE_2__["default"].app,
+        entry: true,
+        children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)(rsmax__WEBPACK_IMPORTED_MODULE_1__.View, {
+            className: _index_css_modules__WEBPACK_IMPORTED_MODULE_2__["default"].header,
+            children: [
+                /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(rsmax__WEBPACK_IMPORTED_MODULE_1__.Image, {
+                    src: "https://gw.alipayobjects.com/mdn/rms_b5fcc5/afts/img/A*OGyZSI087zkAAAAAAAAAAABkARQnAQ",
+                    className: _index_css_modules__WEBPACK_IMPORTED_MODULE_2__["default"].logo,
+                    alt: "logo"
+                }),
+                /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)(rsmax__WEBPACK_IMPORTED_MODULE_1__.View, {
+                    className: _index_css_modules__WEBPACK_IMPORTED_MODULE_2__["default"].text,
+                    children: [
+                        "\u7F16\u8F91 ",
+                        /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(rsmax__WEBPACK_IMPORTED_MODULE_1__.Text, {
+                            className: _index_css_modules__WEBPACK_IMPORTED_MODULE_2__["default"].path,
+                            children: "src/pages/index/index.js"
+                        }),
+                        " \u5F00\u59CB"
+                    ]
+                })
+            ]
+        })
+    });
+};
+
+
+}),
+
+}]);
