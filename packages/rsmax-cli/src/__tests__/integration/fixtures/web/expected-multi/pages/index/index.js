893ac5e6a16385bd53b01d1881805e83b3777689
watsonhaw5566 <348748267@qq.com>
2025-06-13 15:27:13 +0800

    test(cli): 更新测试文件

---
+++ packages/rsmax-cli/src/__tests__/integration/fixtures/web/expected-multi/pages/index/index.js
@@ -0,0 +1,156 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["1"], {
+0: (function (__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+__webpack_require__.r(__webpack_exports__);
+/* ESM import */var _rsmax_web_assets_normalize_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
+/* ESM import */var _rsmax_web_assets_app_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(2);
+/* ESM import */var _app__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(3);
+/* ESM import */var _Users_wangjue_WebstormProjects_rspack_rsmax_packages_rsmax_cli_src_tests_integration_fixtures_web_src_pages_index_index_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(5);
+/* ESM import */var _rsmax_web__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(9);
+
+
+
+
+
+(0,_rsmax_web__WEBPACK_IMPORTED_MODULE_4__.bootstrapMpa)({
+    appComponent: _app__WEBPACK_IMPORTED_MODULE_2__["default"],
+    pageComponent: _Users_wangjue_WebstormProjects_rspack_rsmax_packages_rsmax_cli_src_tests_integration_fixtures_web_src_pages_index_index_js__WEBPACK_IMPORTED_MODULE_3__["default"],
+    page: {
+        "route": "pages/index/index",
+        "config": {}
+    },
+    appConfig: {
+        "pages": [
+            "pages/index/index",
+            "pages/index/index2"
+        ],
+        "window": {
+            "navigationBarTitleText": "Rsmax Ali Template",
+            "navigationBarBackgroundColor": "#282c34"
+        }
+    },
+    plugins: []
+});
+
+
+}),
+5: (function (__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+__webpack_require__.r(__webpack_exports__);
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* export default binding */ __WEBPACK_DEFAULT_EXPORT__)
+});
+/* ESM import */var rsmax__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(6);
+/* ESM import */var rsmax__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(rsmax__WEBPACK_IMPORTED_MODULE_0__);
+/* ESM import */var _index_css_modules__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(7);
+/* ESM import */var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(8);
+/* ESM import */var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__);
+
+
+
+var arr = [
+    1,
+    2,
+    3,
+    4
+];
+var SW = function SW() {
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(rsmax__WEBPACK_IMPORTED_MODULE_0__.Swiper, {
+        style: {
+            height: '300px'
+        },
+        class: "demo-swiper",
+        previousMargin: "10px",
+        nextMargin: "10px",
+        children: arr.map(function(it) {
+            return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(rsmax__WEBPACK_IMPORTED_MODULE_0__.SwiperItem, {
+                children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)(rsmax__WEBPACK_IMPORTED_MODULE_0__.View, {
+                    children: [
+                        "laladsadla ",
+                        it,
+                        /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)(rsmax__WEBPACK_IMPORTED_MODULE_0__.View, {
+                            children: [
+                                "lalala ",
+                                it
+                            ]
+                        }),
+                        /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)(rsmax__WEBPACK_IMPORTED_MODULE_0__.View, {
+                            children: [
+                                "lalala ",
+                                it
+                            ]
+                        }),
+                        /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)(rsmax__WEBPACK_IMPORTED_MODULE_0__.View, {
+                            children: [
+                                "lalala ",
+                                it
+                            ]
+                        })
+                    ]
+                })
+            });
+        })
+    });
+};
+/* ESM default export */ function __WEBPACK_DEFAULT_EXPORT__(props) {
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)(rsmax__WEBPACK_IMPORTED_MODULE_0__.View, {
+        className: _index_css_modules__WEBPACK_IMPORTED_MODULE_1__["default"].app,
+        entry: true,
+        children: [
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)(rsmax__WEBPACK_IMPORTED_MODULE_0__.View, {
+                className: _index_css_modules__WEBPACK_IMPORTED_MODULE_1__["default"].header,
+                children: [
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(rsmax__WEBPACK_IMPORTED_MODULE_0__.Image, {
+                        src: "https://gw.alipayobjects.com/mdn/rms_b5fcc5/afts/img/A*OGyZSI087zkAAAAAAAAAAABkARQnAQ",
+                        className: _index_css_modules__WEBPACK_IMPORTED_MODULE_1__["default"].logo,
+                        alt: "logo"
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)(rsmax__WEBPACK_IMPORTED_MODULE_0__.View, {
+                        className: _index_css_modules__WEBPACK_IMPORTED_MODULE_1__["default"].text,
+                        children: [
+                            "\u7F16\u8F91",
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(rsmax__WEBPACK_IMPORTED_MODULE_0__.Text, {
+                                className: _index_css_modules__WEBPACK_IMPORTED_MODULE_1__["default"].path,
+                                children: "src/pages/index/index.js"
+                            }),
+                            " \u5F00\u59CB"
+                        ]
+                    })
+                ]
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)(rsmax__WEBPACK_IMPORTED_MODULE_0__.Label, {
+                children: [
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(rsmax__WEBPACK_IMPORTED_MODULE_0__.Checkbox, {}),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(rsmax__WEBPACK_IMPORTED_MODULE_0__.Text, {
+                        children: "check"
+                    })
+                ]
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(SW, {})
+        ]
+    });
+};
+
+
+}),
+15: (function (module) {
+module.exports = require("react");
+
+}),
+8: (function (module) {
+module.exports = require("react/jsx-runtime");
+
+}),
+6: (function (module) {
+module.exports = require("rsmax");
+
+}),
+
+},function(__webpack_require__) {
+var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId) }
+__webpack_require__.O(0, ["0",], function() {
+        return __webpack_exec__(0);
+      });
+var __webpack_exports__ = __webpack_require__.O();
+
+}
+]);
