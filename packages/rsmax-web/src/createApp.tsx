87ad3d600b037aabdd6489ac1439336830dfc29e
watsonhaw5566 <348748267@qq.com>
2025-06-16 21:38:31 +0800

    fix(web): 修复路由缓存问题

--- packages/rsmax-web/src/createApp.tsx
+++ packages/rsmax-web/src/createApp.tsx
@@ -3,6 +3,7 @@ import { PluginDriver, RuntimeOptions } from '@rsmax/framework-shared';
 import type { History } from 'history';
 import React from 'react';
 import { HashRouter, Navigate, Route, Routes, useLocation } from 'react-router-dom';
+import { KeepAlive, KeepAliveProvider } from './KeepAlive';
 import { TabBar } from './TabBar';
 import createAppConfig from './createAppConfig';
 import createPageConfig from './createPageConfig';
@@ -32,35 +33,41 @@ export default function createApp(options: BootstrapOptions, history: History) {
       }}
     >
       <AppConfig>
-        <Routes>
-          <Route path="/" element={<Navigate to={`/${pages[0]?.route}`} replace />} />
-          {pages.map((page, i) => {
-            return (
-              <Route
-                key={page.route}
-                path={`/${page.route}`}
-                element={React.createElement(pageComponentsHoc[i], {
-                  pageConfig: {
-                    ...appConfig.window,
-                    ...page.config,
-                  },
-                  tabBar: appConfig.tabBar,
-                })}
-              />
-            );
-          })}
-          <Route
-            path="*"
-            element={
-              /*找不到路由时重定向到首页*/
-              <Navigate to={`/${pages[0]?.route}`} replace />
-            }
-          />
-        </Routes>
-        {appConfig.tabBar?.items && appConfig.tabBar.items.length > 0 && (
-          <TabBar history={history} config={appConfig.tabBar} />
-        )}
-        {process.env.NODE_ENV === 'development' && <LogLocation />}
+        <KeepAliveProvider>
+          <Routes>
+            <Route path="/" element={<Navigate to={`/${pages[0]?.route}`} replace />} />
+            {pages.map((page, i) => {
+              // 合并页面配置
+              const pageConfig = {
+                ...appConfig.window,
+                ...page.config,
+              };
+
+              // 默认所有页面都开启keepAlive
+              const pageElement = React.createElement(pageComponentsHoc[i], {
+                pageConfig,
+                tabBar: appConfig.tabBar,
+              });
+
+              return (
+                <Route
+                  key={page.route}
+                  path={`/${page.route}`}
+                  element={<KeepAlive cacheKey={`/${page.route}`}>{pageElement}</KeepAlive>}
+                />
+              );
+            })}
+            <Route
+              path="*"
+              element={
+                /*找不到路由时重定向到首页*/
+                <Navigate to={`/${pages[0]?.route}`} replace />
+              }
+            />
+          </Routes>
+          {appConfig.tabBar?.items && appConfig.tabBar.items.length > 0 && <TabBar config={appConfig.tabBar} />}
+          {process.env.NODE_ENV === 'development' && <LogLocation />}
+        </KeepAliveProvider>
       </AppConfig>
     </HashRouter>
   );
