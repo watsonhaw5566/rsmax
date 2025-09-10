32aefb660a5d445046364b431cbb27539812dcdf
watsonhaw5566 <watsonhaw@hotmail.com>
2025-09-10 16:53:53 +0800

    test(cli): 打开 web 构建集成测试 (#39)
    * test(cli): 打开 web 构建集成测试
    
    ---------
    
    Co-authored-by: watsonhaw5566 <348748267@qq.com>

---
+++ packages/rsmax-cli/src/__tests__/integration/fixtures/web/expected/pages/index/index.js
@@ -0,0 +1,102 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["1"], {
+192: (function (__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+__webpack_require__.r(__webpack_exports__);
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* export default binding */ __WEBPACK_DEFAULT_EXPORT__)
+});
+/* ESM import */var rsmax__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(193);
+/* ESM import */var rsmax__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(rsmax__WEBPACK_IMPORTED_MODULE_0__);
+/* ESM import */var _index_css_modules__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(194);
+/* ESM import */var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(195);
+/* ESM import */var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__);
+
+
+
+var arr = [
+    1,
+    2,
+    3,
+    4
+];
+var SW = function SW() {
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(rsmax__WEBPACK_IMPORTED_MODULE_0__.Swiper, {
+        style: {
+            height: '300px'
+        },
+        class: "demo-swiper",
+        previousMargin: "10px",
+        nextMargin: "10px",
+        children: arr.map(function(it) {
+            return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(rsmax__WEBPACK_IMPORTED_MODULE_0__.SwiperItem, {
+                children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)(rsmax__WEBPACK_IMPORTED_MODULE_0__.View, {
+                    children: [
+                        "laladsadla ",
+                        it,
+                        /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)(rsmax__WEBPACK_IMPORTED_MODULE_0__.View, {
+                            children: [
+                                "lalala ",
+                                it
+                            ]
+                        }),
+                        /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)(rsmax__WEBPACK_IMPORTED_MODULE_0__.View, {
+                            children: [
+                                "lalala ",
+                                it
+                            ]
+                        }),
+                        /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)(rsmax__WEBPACK_IMPORTED_MODULE_0__.View, {
+                            children: [
+                                "lalala ",
+                                it
+                            ]
+                        })
+                    ]
+                })
+            });
+        })
+    });
+};
+/* ESM default export */ function __WEBPACK_DEFAULT_EXPORT__(props) {
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)(rsmax__WEBPACK_IMPORTED_MODULE_0__.View, {
+        className: _index_css_modules__WEBPACK_IMPORTED_MODULE_1__["default"].app,
+        entry: true,
+        children: [
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)(rsmax__WEBPACK_IMPORTED_MODULE_0__.View, {
+                className: _index_css_modules__WEBPACK_IMPORTED_MODULE_1__["default"].header,
+                children: [
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(rsmax__WEBPACK_IMPORTED_MODULE_0__.Image, {
+                        src: "https://gw.alipayobjects.com/mdn/rms_b5fcc5/afts/img/A*OGyZSI087zkAAAAAAAAAAABkARQnAQ",
+                        className: _index_css_modules__WEBPACK_IMPORTED_MODULE_1__["default"].logo,
+                        alt: "logo"
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)(rsmax__WEBPACK_IMPORTED_MODULE_0__.View, {
+                        className: _index_css_modules__WEBPACK_IMPORTED_MODULE_1__["default"].text,
+                        children: [
+                            "\u7F16\u8F91",
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(rsmax__WEBPACK_IMPORTED_MODULE_0__.Text, {
+                                className: _index_css_modules__WEBPACK_IMPORTED_MODULE_1__["default"].path,
+                                children: "src/pages/index/index.js"
+                            }),
+                            " \u5F00\u59CB"
+                        ]
+                    })
+                ]
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)(rsmax__WEBPACK_IMPORTED_MODULE_0__.Label, {
+                children: [
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(rsmax__WEBPACK_IMPORTED_MODULE_0__.Checkbox, {}),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(rsmax__WEBPACK_IMPORTED_MODULE_0__.Text, {
+                        children: "check"
+                    })
+                ]
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(SW, {})
+        ]
+    });
+};
+
+
+}),
+
+}]);
--- packages/rsmax-web/rstest.config.js
+++ packages/rsmax-web/rstest.config.js
@@ -3,4 +3,5 @@ import { defineConfig } from '@rstest/core';
 export default defineConfig({
   globals: true,
   testEnvironment: 'happy-dom',
+  exclude: ['cjs/', 'esm/', 'node_modules/'],
 });
