63a36510e8d6cb29f987ad260703b378a8e88eb2
watsonhaw5566 <348748267@qq.com>
2025-11-11 23:41:11 +0800

    test(cli): 开启 build 测试

--- packages/rsmax-cli/src/__tests__/integration/build.test.ts
+++ packages/rsmax-cli/src/__tests__/integration/build.test.ts
@@ -1,6 +1,6 @@
 import { testBuildApp } from './helpers/runTest';
 
-describe.skip('build mini app', () => {
+describe('build mini app', () => {
   testBuildApp('ali');
   testBuildApp('toutiao', 'toutiao');
   testBuildApp('resolve-platform-ext');
