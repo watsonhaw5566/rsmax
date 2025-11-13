89bcd6a55f580707f23f434c59d0f283a44d2597
watsonhaw5566 <watsonhaw@hotmail.com>
2025-11-13 22:41:14 +0800

    refactor(toutiao/ali): 重构头条、阿里小程序平台接口导出 (#63)
    * refactor(toutiao): 重构头条平台导出
    ---------
    
    Co-authored-by: watsonhaw5566 <348748267@qq.com>

--- packages/rsmax-one/src/api/index.toutiao.ts
+++ packages/rsmax-one/src/api/index.toutiao.ts
@@ -1,1 +1,8 @@
-export { navigateTo, navigateBack, redirectTo, reLaunch, switchTab } from '@rsmax/toutiao';
+import { promisify } from '@rsmax/framework-shared';
+
+declare const tt: any;
+export const navigateTo = promisify(tt.navigateTo);
+export const navigateBack = promisify(tt.navigateBack);
+export const redirectTo = promisify(tt.redirectTo);
+export const reLaunch = promisify(tt.reLaunch);
+export const switchTab = promisify(tt.switchTab);
