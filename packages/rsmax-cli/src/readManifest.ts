911f3a12c26a2ad1ae2a89c9dec5831be44fd58d
watsonhaw5566 <348748267@qq.com>
2025-11-14 12:02:40 +0800

    chore(cli): 使用 jiti 代替 @swc-node/register

--- packages/rsmax-cli/src/readManifest.ts
+++ packages/rsmax-cli/src/readManifest.ts
@@ -2,10 +2,10 @@ import * as fs from 'node:fs';
 import path from 'node:path';
 import type { Platform } from '@rsmax/types';
 
-function readTypescriptManifest(path: string, target: Platform) {
-  require('@swc-node/register');
-  delete require.cache[require.resolve(path)];
-  return require(path)[target] || require(path).default || require(path);
+function readTypescriptManifest(filePath: string, target: Platform) {
+  const j = require('jiti')(process.cwd(), { cache: false });
+  const mod = j(filePath);
+  return mod[target] || mod.default || mod;
 }
 
 function readJavascriptManifest(path: string, target: Platform) {
