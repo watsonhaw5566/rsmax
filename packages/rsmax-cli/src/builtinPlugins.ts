5b7ca01f239bac886bcfd43f01dfd11cd79b3598
watsonhaw5566 <348748267@qq.com>
2025-11-15 10:20:15 +0800

    chore(cli): 移除 rsmax-plugin-devtools

--- packages/rsmax-cli/src/builtinPlugins.ts
+++ packages/rsmax-cli/src/builtinPlugins.ts
@@ -1,5 +1,3 @@
-import errorScreen from '@rsmax/plugin-error-screen';
-// import devtools from '@rsmax/plugin-devtools';
 import type { Options, Plugin } from '@rsmax/types';
 
 export const builtinPlugins = (
@@ -8,18 +6,5 @@ export const builtinPlugins = (
   optionKey: string;
   init: (...args: any[]) => Plugin;
 }> => {
-  // const isDev = process.env.NODE_ENV !== 'production';
-  const plugins = [
-    {
-      optionKey: 'errorScreen',
-      init: errorScreen,
-    },
-  ];
-  // if (options.target !== 'web' && isDev && options.devtools) {
-  //   plugins.push({
-  //     optionKey: 'devtools',
-  //     init: devtools,
-  //   });
-  // }
-  return plugins;
+  return [];
 };
--- packages/rsmax-plugin-devtools/index.d.ts
+++
@@ -1,3 +0,0 @@
-declare function devtools(): any;
-
-export default devtools;
