e911868bba7d7a05884281d5813f58e765fd79a6
watsonhaw5566 <348748267@qq.com>
2025-11-16 07:35:13 +0800

    test(cli): 打开小程序插件构建

--- packages/rsmax-cli/src/__tests__/integration/hybrid.test.ts
+++ packages/rsmax-cli/src/__tests__/integration/hybrid.test.ts
@@ -1,7 +1,6 @@
-import { testBuildApp } from './helpers/runTest';
+import { testBuildApp, testBuildMiniPlugin } from './helpers/runTest';
 
 describe.skip('hybrid', () => {
   testBuildApp('hybrid-app');
-  // todo 待修复小程序混合组件
-  // testBuildMiniPlugin('hybrid-mini-plugin');
+  testBuildMiniPlugin('hybrid-mini-plugin');
 });
