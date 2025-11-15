d8b636d9754c3be212a445bc0ef13a9ef210dcb8
watsonhaw5566 <348748267@qq.com>
2025-11-16 00:00:25 +0800

    chore: 移除 component-scripts
    迁移 component-scripts 至 rsmax-plugin 中维护

--- packages/rsmax-component-scripts/src/build/types.ts
+++
@@ -1,6 +0,0 @@
-export interface IArgv {
-  type: string;
-  watch: boolean;
-  config: string | undefined;
-  output: string;
-}
--- packages/rsmax-component-scripts/src/tests/fixture/basic/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Foo } from './Foo';
-// @ts-ignore
-export { default as Greet } from './Greet';
