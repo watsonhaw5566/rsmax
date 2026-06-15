1bbfccb5a1e4cb59c764dd175a795b78a80c4b22
watsonhaw5566 <348748267@qq.com>
2026-06-15 14:57:51 +0800

    chore(deps): 移除 rsmax-plugin-devtools 包(迁移到 plugins 中)

--- packages/rsmax-cli/src/builtinPlugins.ts
+++ packages/rsmax-cli/src/builtinPlugins.ts
@@ -1,5 +1,4 @@
 import errorScreen from '@rsmax/plugin-error-screen';
-// import devtools from '@rsmax/plugin-devtools';
 import type { Options, Plugin } from '@rsmax/types';
 
 export const builtinPlugins = (
@@ -8,18 +7,11 @@ export const builtinPlugins = (
   optionKey: string;
   init: (...args: any[]) => Plugin;
 }> => {
-  // const isDev = process.env.NODE_ENV !== 'production';
   const plugins = [
     {
       optionKey: 'errorScreen',
       init: errorScreen,
     },
   ];
-  // if (options.target !== 'web' && isDev && options.devtools) {
-  //   plugins.push({
-  //     optionKey: 'devtools',
-  //     init: devtools,
-  //   });
-  // }
   return plugins;
 };
--- packages/rsmax-plugin-devtools/typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare const my: any;
-declare const tt: any;
-declare const wx: any;
-declare module 'webpack-inject-plugin';
-declare module '@remax/react-devtools-core';
