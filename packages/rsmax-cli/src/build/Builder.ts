f0f71a6025341279fc1ed66ba7f04c4590f0a141
watsonhaw5566 <348748267@qq.com>
2026-06-16 15:10:50 +0800

    chore(cli): 移除 cfg.plugins[index] = cssPlugin

--- packages/rsmax-cli/src/build/Builder.ts
+++ packages/rsmax-cli/src/build/Builder.ts
@@ -70,16 +70,8 @@ abstract class Builder {
   }
 
   createWebpackCompiler(): Compiler {
-    const cfg = this.webpackConfig;
-
-    const index = cfg.plugins.findIndex((e: any) => e.constructor.name === 'CssExtractRspackPlugin');
-    const cssPlugin = cfg.plugins[index];
-
-    if (cssPlugin) {
-      cfg.plugins[index] = cssPlugin;
-    }
-    // @ts-expect-error
-    return rspack(cfg);
+    // @ts-expect-error rspack 类型兼容问题
+    return rspack(this.webpackConfig);
   }
 }
 
