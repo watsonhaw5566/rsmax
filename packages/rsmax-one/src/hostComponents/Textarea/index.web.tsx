25a6e7e52c2bcd85f7a0011b59440e9ff1e7e5fd
watsonhaw5566 <348748267@qq.com>
2026-06-17 18:58:37 +0800

    chore(one): 使用 react-textarea-autosize 代替 react-autosize-textarea

--- packages/rsmax-one/src/hostComponents/Textarea/index.web.tsx
+++ packages/rsmax-one/src/hostComponents/Textarea/index.web.tsx
@@ -1,32 +1,40 @@
 import clsx from 'clsx';
-import * as React from 'react';
-import TextareaAutoSize from 'react-autosize-textarea';
+import React from 'react';
+import TextareaAutosize from 'react-textarea-autosize';
 import useWebPlaceholderStyle from '../../useWebPlaceholderStyle';
 import { filterProps } from '../../utils/isPlatformSpecifyProp';
 import type { TextareaWebProps } from './props';
 
 const Textarea: React.ForwardRefRenderFunction<any, TextareaWebProps> = (props, ref) => {
-  const { onConfirm, onKeyPress, autoHeight, className, placeholderStyle, ...restProps } = filterProps(props);
+  const { onConfirm, onKeyDown, autoHeight, className, placeholderStyle, ...restProps } = filterProps(props);
   const [placeholderStyleClassName] = useWebPlaceholderStyle(placeholderStyle);
 
-  function handleKeyPress(e: React.KeyboardEvent<HTMLTextAreaElement>) {
+  function handleKeyDown(e: React.KeyboardEvent<HTMLTextAreaElement>) {
     if (e.key === 'Enter' && typeof onConfirm === 'function') {
       onConfirm(e);
     }
 
-    if (typeof onKeyPress === 'function') {
-      onKeyPress(e);
+    if (typeof onKeyDown === 'function') {
+      onKeyDown(e);
     }
   }
 
   const textareaClassName = clsx('remax-textarea', className, placeholderStyleClassName);
 
   if (autoHeight) {
-    // @ts-ignore
-    // todo 需要修正这里
-    return <TextareaAutoSize {...restProps} className={textareaClassName} ref={ref} onKeyPress={handleKeyPress} />;
+    const TextareaAutoSize = TextareaAutosize as unknown as React.ComponentType<
+      React.TextareaHTMLAttributes<HTMLTextAreaElement> & { ref?: React.Ref<HTMLTextAreaElement> }
+    >;
+    return (
+      <TextareaAutoSize
+        {...restProps}
+        className={textareaClassName}
+        ref={ref as React.Ref<HTMLTextAreaElement>}
+        onKeyDown={handleKeyDown}
+      />
+    );
   }
 
-  return <textarea {...restProps} className={textareaClassName} ref={ref} onKeyPress={handleKeyPress} />;
+  return <textarea {...restProps} className={textareaClassName} ref={ref} onKeyDown={handleKeyDown} />;
 };
 export default React.forwardRef(Textarea);
