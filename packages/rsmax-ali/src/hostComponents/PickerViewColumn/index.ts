ae3ab02d6f76b790484b29e5cf81cc744ea64a94
watsonhaw5566 <348748267@qq.com>
2026-07-21 12:09:29 +0800

    refactor(one): 移除 ali/toutiao/wechat 独立包

--- packages/rsmax-ali/src/hostComponents/PickerViewColumn/index.ts
+++
@@ -1,4 +0,0 @@
-import { createHostComponent } from '@rsmax/runtime';
-import type React from 'react';
-
-export const PickerViewColumn = createHostComponent('picker-view-column') as React.ComponentType<any>;
--- packages/rsmax-ali/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './hostComponents';
-export * from './types';
-export * from './api';
